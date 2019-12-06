6baf5b87ea7237ef64d18f8f28a0c2a274a9cd93
Nadine Ju <nadine.jura@online.de>
2019-12-05 23:08:39 -0800

    inquirer prompts set up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,14 @@
-// require npm packages
+/*************************************
+NPM PACKAGES
+*************************************/
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require('console.table');
 
-// connect to MySQL database
+/*************************************
+MYSQL
+*************************************/
+// connect to database
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -18,29 +23,55 @@ var connection = mysql.createConnection({
     database: "bamazon",
 });
 
-connection.connect(function(err) {
-   if (err) {
-        throw err
-    };
-    console.log("connected as id ", connection.threadId);
-    displayProducts()
-    connection.end();
- });
-
 function displayProducts() {
+    console.log("Product Overview:");
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       console.table(res);
     });
-  }  
+};
 
-// prompt user to buy products with inquirer
-// inquirer.prompt([
-    // {
-    //     name: "product_id",
-    //     message: "What is the ID of the product you would like to buy?"
-    // }
-    // }]).then(function (inquirerResponse) {
-    //     console.log("ID: " + inquirerResponse.product_id);
-    // });
+/*************************************
+INQUIRER
+*************************************/
+// prompt user to buy products
+function customerPrompt() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "product_id",
+                message: "What is the ID of the product you are interested in?"
+            }]).then(function (inquirerResponse) {
+                    console.log("You have selected: " + res[inquirerResponse.product_id-1].product_name);
+                inquirer.prompt([
+                    {
+                        name: "product_quantity",
+                        message: "How many would you like to purchase?"
+                    }]).then(function (inquirerResponse) {
+                    console.log("Quantity: " + inquirerResponse.product_quantity);
+                });
+        });
+    });
+};
+
+/*************************************
+MAIN CODE
+*************************************/
+// welcome message
+function displayWelcome() {
+    console.log("Welcome to TravelEasy!");
+    console.log("The Online Store That Makes Packing for Your Travel Needs a Breeze");
+    console.log("---------------------------------------------");
+}
+connection.connect(function(err) {
+    if (err) {
+         throw err
+     };
+     // console.log("connected as id ", connection.threadId);
+     displayWelcome();
+     displayProducts();
+     customerPrompt();
+     connection.end();
+  });
 
